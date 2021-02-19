bc6cd5f0c4dab9c3c78f29106622ce7076c342bd
danishkhangithub <danishkhankd237@gmail.com>
2021-02-19 08:37:15 +0500

    first commit

---
+++ apps/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'', UserViewSet)
+
+urlpatterns = [
+    #path('', include('router.urls')),
+    path('', include(router.urls)),
+
+]
+
